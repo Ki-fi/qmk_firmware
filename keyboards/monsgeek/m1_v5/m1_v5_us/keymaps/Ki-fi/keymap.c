ba20e4464b07d08c2139f30edef6ac958468694a
Ki-fi <k.meekels@gmail.com>
2025-08-22 23:12:40 +0200

    New build created

--- keyboards/monsgeek/m1_v5/m1_v5_us/keymaps/Ki-fi/keymap.c
+++ keyboards/monsgeek/m1_v5/m1_v5_us/keymaps/Ki-fi/keymap.c
@@ -14,15 +14,88 @@ enum layers {
 
 #define ______ HS_BLACK
 
+enum custom_keycodes {
+    OK = SAFE_RANGE,
+    ROCK,
+    NICE,
+    POINT,
+    SMILEY,
+    FINDER,
+    UNDO,
+    SAVE
+};
+
+bool process_record_keymap(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+    case OK:
+        if (record->event.pressed) {
+            SEND_STRING(SS_LALT("D83DDC4D"));
+        } else {
+        }
+        break;
+
+    case ROCK:
+        if (record->event.pressed) {
+            SEND_STRING(SS_LALT("D83EDD18"));
+        } else {
+        }
+        break;
+
+    case NICE:
+        if (record->event.pressed) {
+            SEND_STRING(SS_LALT("D83DDC4C"));
+        } else {
+        }
+        break;
+
+    case POINT:
+        if (record->event.pressed) {
+            SEND_STRING(SS_LALT("D83DDC46"));
+        } else {
+        }
+        break;
+
+    case SMILEY:
+            if (record->event.pressed) {
+                SEND_STRING(SS_LALT("D83DDE42"));
+            } else {
+            }
+            break;
+
+    case FINDER:
+            if (record->event.pressed) {
+                SEND_STRING(SS_LGUI(SS_LALT(" ")));
+            } else {
+            }
+            break;
+
+    case UNDO:
+            if (record->event.pressed) {
+                SEND_STRING(SS_LGUI("z"));
+            } else {
+            }
+            break;
+
+    case SAVE:
+            if (record->event.pressed) {
+                SEND_STRING(SS_LGUI("s"));
+            } else {
+            }
+            break;
+
+    }
+    return true;
+};
+
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_BL] = LAYOUT( /* Base */
-        KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_DEL,   KC_MUTE,
-        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,  KC_HOME,
-        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,  KC_PGUP,
-        KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,   KC_PGDN,
-        KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,  KC_UP,    KC_END,
+        KC_ESC,   OK,       ROCK,     NICE,     POINT,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_DEL,   KC_MUTE,
+        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,  SMILEY,
+        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,  FINDER,
+        KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,   UNDO,
+        KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,  KC_UP,    SAVE,
         KC_LCTL,  KC_LCMD,  KC_LALT,                      KC_SPC,                                 KC_RALT,  MO(_FL),  KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
     [_FL] = LAYOUT( /* Base */
